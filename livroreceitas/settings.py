1f9c2e602ac655a5fc7b09d7425c291e054b9521
Lucas Campbell <lcampbell@id.uff.br>
2022-08-30 14:28:43 -0300

    Criacao do app de usuarios
    Criação do app usuários e registro dos mesmos no banco de dados com validações básicas.

--- livroreceitas/settings.py
+++ livroreceitas/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 INSTALLED_APPS = [
     'pessoas',
     'receitas',
+    'usuarios',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ usuarios/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+
+def cadastro(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('As senhas não correspondem')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print('Usuario ja cadastrado')
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print('Usuario cadastrado com sucesso')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
+
+
+def login(request):
+    return render(request, 'usuarios/login.html')
+
+
+def logout(request):
+    pass
+
+
+def dashboard(request):
+    pass
